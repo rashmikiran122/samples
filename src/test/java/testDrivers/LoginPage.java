443869a74ad644cc14eb96a6cb5d115d20b2d540
Administrator <Administrator@172.31.7.173>
2023-10-18 13:21:57 +0000

    inital project

---
+++ src/test/java/testDrivers/LoginPage.java
@@ -0,0 +1,23 @@
+package testDrivers;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+	//	driver.get("https://accounts.lambdatest.com/register");
+	//	driver.get("https://demo.seleniumeasy.com/table-sort-search-demo.html");
+	//	driver.get("https://the-internet.herokuapp.com/login");
+		driver.get("https://www.lambdatest.com/blog/");
+		
+	//	driver.findElement(By.xpath("")).sendKeys("");
+		
+	//	driver.findElement(By.cssSelector("").sendKeys("");
+		
+
+	}
+
+}
